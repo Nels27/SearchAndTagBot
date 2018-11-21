3a7924717e1cd14d9a186bb9a6a05f88cc522a2d
Nelson R <NelsonRaphael27@gmail.com>
2018-11-21 13:10:11 -0500

    Full functioning interface. Removed unnecessary dictionaries.

--- RPi/controllerInterface/controllerInterfaceV2.py
+++ RPi/controllerInterface/controllerInterfaceV2.py
@@ -11,7 +11,7 @@
 # Copyright © 2015 Clay L. McLeod <clay.l.mcleod@gmail.com>
 #
 # Adjusted by adding a dictionary for each key command on a PS4 controller
-# And added a new method for dealing with actions assigned to each button action
+# Prints exactly what occurs in terms of controller action
 # Copyright © 2017 Nelson Raphael <NelsonRaphael27@gmail.com>
 #
 # Distributed under terms of the MIT license.
@@ -58,9 +58,7 @@ class PS4Controller(object):
             self.button_data = {}
             for i in range(self.controller.get_numbuttons()):
                 self.button_data[i] = False
-                self.button_dataKeyCommandsPress = {0: 'X buttom pressed', 1: 'O button pressed', 2: 'Triangle Button pressed', 3: 'Square Button pressed', 4: 'L1 button pressed', 5: 'R1 button pressed', 6: 'L2 button pressed', 7: 'R2 button pressed', 8: 'Share button pressed', 9: 'Options button pressed' ,10: 'PS4 home button pressed', 11: 'L3 or Left Joystick Pressed', 12: 'R3 or Right Joystick Pressed'}
-                self.button_dataKeyCommandsRelease = {0: 'X buttom released', 1: 'O button released', 2: 'Triangle Button released', 3: 'Square Button released', 4: 'L1 button released', 5: 'R1 button released', 6: 'L2 button released', 7: 'R2 button released', 8: 'Share button released', 9: 'Options button released' ,10: 'PS4 home button released', 11: 'L3 or Left Joystick released', 12: 'R3 or Right Joystick released'}
-
+                
                 #Button Mapping
                 #X - 0
                 #O - 1
