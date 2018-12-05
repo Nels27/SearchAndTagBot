bee123ad5ec3600be44730a52a532df2e778a238
Nelson R <NelsonRaphael27@gmail.com>
2018-12-05 13:22:48 -0500

    Added an updated raspi side code to actuate motors.

--- mbed/mbedSerialReceiverTestCodeJoystick.cpp
+++ mbed/mbedSerialReceiverTestCodeJoystick.cpp
@@ -16,125 +16,49 @@ DigitalOut led1(LED1); // Use this for color filter purposes
 DigitalOut led2(LED2); // Use for left right movement
 DigitalOut led3(LED3); // Use for up down movement
 DigitalOut led4(LED4); // Use for servo rotation
-Motor RW(p22, p7, p6); //  pwm, fwd, rev
-Motor LW(p21, p8, p11);// pwm, fwd, rev
+//Motor RW(p22, p7, p6); //  pwm, fwd, rev
+//Motor LW(p21, p8, p11);// pwm, fwd, rev
 
 
 
 void dev_recv()
 {
-    char temp = 0;
+    char temp = '0';
     //led1 = !led1;
     while(pi.readable()) {
         temp = pi.getc();
         //string tempString(11, temp);
         pi.putc(temp);
-        if (isblank(temp) == true) { print('Error with communciation')};
+        if (isblank(temp) == '1')  led2 = 1;
 
-        elseif (isblank(temp) == false) {print('Connection successful')};
-            if (temp == "Up button pressed") {
+        else if (isblank(temp) == '0') led3 = 1;
 
+            if(temp == 'e') led1 = 1;
+
+            else if(temp == 'E') led1 = 0;
+
+            else if(temp == 'f') led2 = 1;
+
+            else if(temp == 'F') led2 = 0;
+
+            else if(temp == 'g') led3 = 1;
+
+            else if(temp == 'G') led3 = 0;
+
+            else if(temp == 'h') led4 = 1;
+
+            else if(temp == 'H') led4 = 0;
+
+            else if(temp == 'z'){
+                led1 = 0;
+                led2 = 0;
+                led3 = 0;
+                led4 = 0;
             }
-
-            elseif (temp == "Up button let go"){
-
-            }
-
-            elseif (temp == "Down button pressed") {
-
-            }
-
-            elseif (temp == "Down button let go"){
-
-            }
-
-            elseif (temp == "Left button pressed") {
-
-            }
-
-            elseif (temp == "Left button let go"){
-
-            }
-
-            elseif (temp == "Right button pressed") {
-
-            }
-
-            elseif (temp == "Right button let go"){
-
-            }
-
-            elseif (temp == "Left Joystick moved up") {
-
-            }
-
-            elseif (temp == "Left Joystick moved down"){
-
-            }
-
-            elseif (temp == "Left Joystick moved left") {
-
-            }
-
-            elseif (temp == "Left Joystick moved right"){
-
-            }
-
-            elseif (temp == "Right Joystick moved up") {
-
-            }
-
-            elseif (temp == "Right Joystick moved down"){
-
-            }
-
-            elseif (temp == "Right Joystick moved left") {
-
-            }
-
-            elseif (temp == "Right Joystick moved right"){
-
-            }
-
-            elseif (temp == "X button pressed") {
-
-            }
-
-            elseif (temp == "X button let go"){
-
-            }
-
-            elseif (temp == "O button pressed") {
-
-            }
-
-            elseif (temp == "O button let go"){
-
-            }
-
-            elseif (temp == "Square button pressed") {
-
-            }
-
-            elseif (temp == "Square button let go"){
-
-            }
-
-            elseif (temp == "Triangle button pressed") {
-
-            }
-
-            elseif (temp == "Triangle button let go"){
-
-            }
-
-
-
-
-
 
     }
 }
+
 int main()
 {
     pi.baud(9600);
